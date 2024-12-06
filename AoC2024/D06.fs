14e7e0a17de6f97fb9efe9e7b2439b9f4656b844
Iain M Norman <iain@norman.org>
2024-12-06 23:08:45 +0000

    6

--- AoC2024/D06.fs
+++ AoC2024/D06.fs
@@ -3,62 +3,79 @@ module D06
 open System.Collections.Generic
 
 let readMap (puzzleInput: string) =
-    puzzleInput.Trim().Split '\n'
-    |> Array.map (fun line -> line.ToCharArray())
+    puzzleInput.Trim().Split '\n' |> Array.map (fun line -> line.ToCharArray())
 
 let turnRight currentDirection =
-    match currentDirection with
-    | '^' -> '>'
-    | '>' -> 'v'
-    | 'v' -> '<'
-    | '<' -> '^'
-    | _ -> failwith "Invalid direction"
+    (currentDirection + 1) % 4
 
 let moveForward (x, y) direction =
     match direction with
-    | '^' -> (x - 1, y)
-    | 'v' -> (x + 1, y)
-    | '<' -> (x, y - 1)
-    | '>' -> (x, y + 1)
+    | 0 -> (x - 1, y)
+    | 2 -> (x + 1, y)
+    | 3 -> (x, y - 1)
+    | 1 -> (x, y + 1)
     | _ -> failwith "Invalid direction"
 
 let part1 (labMap: char array array) obsR obsC gp gd =
     let mutable looping = false
-    let visitedPositions = HashSet<int * int * char>()
+
     let mutable guardPosition = gp
     let mutable guardDirection = gd
     let rows = labMap.Length
     let cols = labMap[0].Length
+
+    let visitedPositions = HashSet<(int * int)>()
 
     let mutable continuePatrol = true
 
     while continuePatrol do
         let r, c = guardPosition
 
-        if visitedPositions.Contains(r, c, guardDirection) then
-            looping <- true
-            continuePatrol <- false
+        visitedPositions.Add(r, c) |> ignore
 
-        visitedPositions.Add(r, c, guardDirection) |> ignore
-
-        // Check next position
         let nextPosition = moveForward guardPosition guardDirection
         let nextR, nextC = nextPosition
 
         if nextR < 0 || nextR >= rows || nextC < 0 || nextC >= cols then
             continuePatrol <- false
         else if labMap[nextR].[nextC] <> '#' && (nextR, nextC) <> (obsR, obsC) then
-            // Move forward if no obstacle
             guardPosition <- nextPosition
         else
-            // Turn right if obstacle or out of bounds
             guardDirection <- turnRight guardDirection
 
     visitedPositions, looping
 
+let checkForLoop (labMap: char array array) obsR obsC gp gd rows cols =
+    let mutable looping = false
+    let mutable guardPosition = gp
+    let mutable guardDirection = gd
+    let visitedPositions = Array3D.zeroCreate<bool> rows cols 4
+    let mutable continuePatrol = true
+
+    while continuePatrol do
+        let r, c = guardPosition
+            
+        visitedPositions.[r, c, guardDirection] <- true
+        let nextPosition = moveForward guardPosition guardDirection
+        let nextR, nextC = nextPosition
+        if nextR < 0 || nextR >= rows || nextC < 0 || nextC >= cols then
+            continuePatrol <- false
+        else if labMap[nextR].[nextC] <> '#' && (nextR, nextC) <> (obsR, obsC) then
+            guardPosition <- nextPosition
+        else
+            if visitedPositions[r, c, guardDirection] then
+                looping <- true
+                continuePatrol <- false
+            guardDirection <- turnRight guardDirection
+            
+    looping
+
 let part2 (labMap: char array array) gp gd vps =
+    let rows = labMap.Length
+    let cols = labMap[0].Length
+
     vps
-    |> Array.filter (fun vp -> snd (part1 labMap (fst vp) (snd vp) gp gd))
+    |> Array.filter (fun vp -> checkForLoop labMap (fst vp) (snd vp) gp gd rows cols)
     |> Array.length
 
 let solve input =
@@ -66,26 +83,29 @@ let solve input =
     let rows = labMap.Length
     let cols = labMap[0].Length
     let mutable guardPosition = (0, 0)
-    let mutable guardDirection = ' '
+    let mutable guardDirection = -1
 
     for r in 0 .. rows - 1 do
         for c in 0 .. cols - 1 do
             match labMap[r][c] with
-            | '^'
-            | 'v'
-            | '<'
+            | '^' ->
+                guardPosition <- (r, c)
+                guardDirection <- 0
+            | 'v' ->
+                guardPosition <- (r, c)
+                guardDirection <- 2
+            | '<' ->
+                guardPosition <- (r, c)
+                guardDirection <- 3
             | '>' ->
                 guardPosition <- (r, c)
-                guardDirection <- labMap[r][c]
+                guardDirection <- 1
 
             | _ -> ()
 
     let p1 =
-        fst (part1 labMap -1 -1 guardPosition guardDirection)
-        |> Seq.map (fun (x, y, _) -> (x, y))
-        |> Seq.distinct
-        |> Seq.toArray
-    
+        fst (part1 labMap -1 -1 guardPosition guardDirection) |> Seq.toArray
+
     let p2 = part2 labMap guardPosition guardDirection p1
 
     p1.Length, p2
