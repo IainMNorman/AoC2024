25a30c5f60a881eba1ef65466ff45093415690b2
Iain M Norman <iain@norman.org>
2024-12-09 22:39:30 +0000

    9 done

--- AoC2024/Days/D09.fs
+++ AoC2024/Days/D09.fs
@@ -6,30 +6,73 @@ let parseInput (input: string) =
     |> Seq.chunkBySize 2
     |> Seq.mapi (fun i chunk ->
         match chunk with
-        | [|f;s|] -> Seq.append
-                         (Seq.replicate f i)
-                         (Seq.replicate s -1)
-        | [|f|] -> Seq.replicate f i
-        | _ -> Seq.empty) 
+        | [| f; s |] -> Seq.append (Seq.replicate f i) (Seq.replicate s -1)
+        | [| f |] -> Seq.replicate f i
+        | _ -> Seq.empty)
     |> Seq.concat
     |> Seq.toArray
-    
+
 let checksum (disk: int array) =
     disk
+    |> Array.map int64
     |> Array.mapi (fun i c ->
         match c with
-        | -1 -> 0
-        | _ -> (c |> string |> int) * i)
-    |> Array.map int64
+        | -1L -> 0L
+        | _ -> (c * (int64 i))
+        )
     |> Array.sum
-    
+
+let getIndexAndCount (fileId: int) (disk: int array) =
+    let firstIndex = disk |> Array.findIndex (fun id -> id = fileId)
+    let count = disk |> Array.fold (fun acc id -> if id = fileId then acc + 1 else acc) 0
+    (firstIndex, count)
+
+let findContiguousSpaceToLeft currentStart size (disk: int array) =
+    let rec findIndex start count =
+        if start + size > currentStart + size then
+            None
+        elif count = size then
+            Some(start - size)
+        elif disk[start] = -1 then
+            findIndex (start + 1) (count + 1)
+        else
+            findIndex (start + 1) 0
+
+    findIndex 0 0
+
 let part1 input =
     let disk = parseInput input
     let spaceCount = disk |> Array.filter (fun i -> i = -1) |> Array.length
-    for i in 1 .. spaceCount do
+
+    for i in 1..spaceCount do
         let indexOfNextSpace = disk |> Array.findIndex (fun x -> x = -1)
         disk[indexOfNextSpace] <- disk[disk.Length - i]
         disk[disk.Length - i] <- -1
+
     checksum disk
 
-let solve input = part1 input, 0
+let part2 input =
+    let disk = parseInput input
+
+    let descendingValues = seq { (disk |> Array.max) .. -1 .. 0 }
+    descendingValues
+    |> Seq.iter (fun v ->
+        let sourceRange = getIndexAndCount v disk
+        let space = findContiguousSpaceToLeft (fst sourceRange) (snd sourceRange) disk
+        if space.IsSome then
+            Array.fill disk (fst sourceRange) (snd sourceRange) -1
+            Array.fill disk space.Value (snd sourceRange) v)
+    
+    disk
+    |> checksum
+        
+
+let solve input =
+    //let test = part2 "2333133121414131499"
+    part1 input, part2 input
+
+// part1 "2333133121414131402"
+// part2 "2333133121414131499" 
+// parseInput "2333133121414131402"
+
+//6398097037768
