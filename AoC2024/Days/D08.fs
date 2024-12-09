d2bfc583ad5c7b0102340ec2d551abdd50bedc9f
Iain M Norman <iain@norman.org>
2024-12-09 08:36:44 +0000

    9

--- AoC2024/Days/D08.fs
+++ AoC2024/Days/D08.fs
@@ -1,4 +1,90 @@
 module D08
 
+let stringToBytesASCII (input: string) =
+    System.Text.Encoding.ASCII.GetBytes(input)
 
-let solve lines = 0,0
+let parseAntennas lines =
+    lines
+    |> Array.mapi (fun row line ->
+        stringToBytesASCII line
+        |> Array.mapi (fun col value ->
+            match value with
+            | 46uy -> None
+            | _ -> Some((row, col), value)))
+    |> Array.concat
+    |> Array.choose id
+    |> Map.ofArray // immutable map of input without periods
+
+let getVector ((ax, ay), (bx, by)) = (bx - ax, by - ay)
+
+let addVector (x, y) (dx, dy) = (x + dx, y + dy)
+
+let subVector (x, y) (dx, dy) = (x - dx, y - dy)
+
+let inBounds w h (x, y) = x >= 0 && x < w && y >= 0 && y < h
+
+let pairToAntiNodePair pair =
+    let vector = getVector pair
+    (subVector (fst pair) vector, addVector (snd pair) vector)
+
+let pairToAllAntiNodes w h pair =
+    let vector = getVector pair
+
+    let collectPoints startPoint stepFn =
+        List.unfold (fun p ->
+            if inBounds w h p then Some(p, stepFn p) else None
+        ) startPoint
+
+    collectPoints (fst pair) (fun p -> subVector p vector) @
+    collectPoints (snd pair) (fun p -> addVector p vector)
+    
+let part1 lines =
+    parseAntennas lines
+    |> Seq.groupBy _.Value
+    |> Seq.map (fun (_, group) ->
+        let keys = group |> Seq.map _.Key |> Seq.toList
+
+        keys
+        |> Seq.collect (fun i -> keys |> List.skipWhile ((<>) i) |> List.tail |> Seq.map (fun j -> (i, j))))
+    |> Seq.concat
+    |> Seq.map pairToAntiNodePair
+    |> Seq.collect (fun (a, b) -> [ a; b ])
+    |> Seq.distinct
+    |> Seq.filter (inBounds lines[0].Length lines.Length)
+    |> Seq.length
+    
+let part2 lines =
+    parseAntennas lines
+    |> Seq.groupBy _.Value
+    |> Seq.map (fun (_, group) ->
+        let keys = group |> Seq.map _.Key |> Seq.toList
+    
+        keys
+        |> Seq.collect (fun i -> keys |> List.skipWhile ((<>) i) |> List.tail |> Seq.map (fun j -> (i, j))))
+    |> Seq.concat
+    |> Seq.map (fun p -> pairToAllAntiNodes lines[0].Length lines.Length p )
+    |> Seq.concat
+    |> Seq.distinct
+    |> Seq.filter (inBounds lines[0].Length lines.Length)
+    |> Seq.length
+
+let solve lines = part1 lines, part2 lines
+
+let test =
+    part1 (
+        """
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+"""
+            .Split([| '\n' |], System.StringSplitOptions.RemoveEmptyEntries)
+    )
--- AoC2024/Days/D09.fs
+++ AoC2024/Days/D09.fs
@@ -1,2 +1,35 @@
-module AoC2024.Days.D09
+module D09
 
+let parseInput (input: string) =
+    input
+    |> Seq.map (fun c -> c |> string |> int)
+    |> Seq.chunkBySize 2
+    |> Seq.mapi (fun i chunk ->
+        match chunk with
+        | [|f;s|] -> Seq.append
+                         (Seq.replicate f i)
+                         (Seq.replicate s -1)
+        | [|f|] -> Seq.replicate f i
+        | _ -> Seq.empty) 
+    |> Seq.concat
+    |> Seq.toArray
+    
+let checksum (disk: int array) =
+    disk
+    |> Array.mapi (fun i c ->
+        match c with
+        | -1 -> 0
+        | _ -> (c |> string |> int) * i)
+    |> Array.map int64
+    |> Array.sum
+    
+let part1 input =
+    let disk = parseInput input
+    let spaceCount = disk |> Array.filter (fun i -> i = -1) |> Array.length
+    for i in 1 .. spaceCount do
+        let indexOfNextSpace = disk |> Array.findIndex (fun x -> x = -1)
+        disk[indexOfNextSpace] <- disk[disk.Length - i]
+        disk[disk.Length - i] <- -1
+    checksum disk
+
+let solve input = part1 input, 0
